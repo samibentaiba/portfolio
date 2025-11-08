c50522d4dd3c143e79f8538ee77a766de2dcb7f5
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-11-08 13:31:13 +0100

    debuigging SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import { Experiences } from "@/components/experiences";
 import { Projects } from "@/components/projects";
 import { Contact } from "@/components/contact"
 import Hero from "@/components/hero";
-export const metadata = {
-  title: "Bentaidev",
-  description: "I'm a passionate and fast-learning software engineer and student who, within 3 years, has mastered full-stack web development using both JavaScript , Java and PHP. Currently, I'm expanding my skills into desktop and mobile application development, along with a deep focus on building software services across multiple platforms. My drive to continuously learn and adapt allows me to tackle new technologies quickly and effectively.",
-};
+
 
 export default async function Home() {
   return (
